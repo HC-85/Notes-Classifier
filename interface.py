95d3d17460bdb74cd2b212aef333da623d54a8a1
HC-85 <hugoce.85@gmail.com>
2024-02-10 18:50:50 -0600

    first commit

---
+++ interface.py
@@ -0,0 +1,302 @@
+import curses
+from curses.textpad import rectangle
+from utils import (explore_dir, check_md_tags_headers, inspect_md_tags, demo_suggest_tags, write_tags, 
+                   write_header, remove_tags)
+
+
+def disp_suggest_tags(stdscr, vault, mds, ticked, n_suggestions=3):
+    mds = [md for md, tick in zip(mds, ticked) if tick]
+    tag_ticks = [[False for _ in range(n_suggestions)] for _ in mds]
+    tags = []
+    for md in mds:
+        tags.append(demo_suggest_tags(vault, md, n_suggestions))
+
+    stdscr.clear()
+    stdscr.addstr(0, 1, 'Current Vault: '+ vault, curses.A_UNDERLINE)
+    stdscr.addstr(1, 1, "Select tags to add:", colors['YELLOW_AND_BLACK'])
+    stdscr.addstr(curses.LINES - 3, len("[s]:select/deselect") + 2, "[a]:add to file")
+    stdscr.addstr(curses.LINES - 3, 1 , "[s]:select/deselect")
+    stdscr.addstr(curses.LINES - 2, 1 , "[t]:back")
+    stdscr.addstr(curses.LINES - 2, curses.COLS - 10, "[q]:quit")
+
+    selection_md_id = 0
+    selection_tag_id = 0
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "KEY_UP":
+            selection_md_id-=1
+        elif key == "KEY_DOWN":
+            selection_md_id+=1
+        elif key == "KEY_LEFT":
+            selection_tag_id-=1
+        elif key == "KEY_RIGHT":
+            selection_tag_id+=1
+
+        elif key == "s":
+            tag_ticks[selection_md_id][selection_tag_id] = not tag_ticks[selection_md_id][selection_tag_id]
+
+        elif key == "t":
+            disp_md_selection(stdscr, vault, ticked)
+
+        elif key == "q":
+            quit()
+            
+
+        if len(mds)>0:
+            selection_tag_id = selection_tag_id % n_suggestions
+            selection_md_id = selection_md_id % len(mds)
+            row = 1
+            md_id = 0
+            for md in mds:
+                row+=1
+                stdscr.addstr(row, 1, f"{md[:-3]}:", colors["CYAN_AND_BLACK"])
+                row+=1
+                stdscr.addstr(row, 1, "")
+                
+                tag_id = 0
+                for tag in tags[md_id]:
+                    if (selection_tag_id==tag_id)&(selection_md_id==md_id):
+
+                        if tag_ticks[md_id][tag_id]:
+                            color = colors["GREEN_AND_WHITE"]
+                        else:
+                            color = colors["RED_AND_WHITE"]
+                    else:
+                        if tag_ticks[md_id][tag_id]:
+                            color = colors["GREEN_AND_BLACK"]
+                        else:
+                            color = colors["RED_AND_BLACK"]
+                    
+                    stdscr.addstr(f"#{tag}", color)
+                    stdscr.addstr(f" ")
+                    tag_id+=1
+                row+=1
+                md_id+=1
+
+        stdscr.refresh()
+
+#TODO: check if header is at the end of the file
+def disp_check_headers(stdscr, vault, mds, ticked):
+    mds = [md for md, tick in zip(mds, ticked) if tick]
+
+    stdscr.clear()
+    stdscr.addstr(0, 1, 'Current Vault: '+ vault, curses.A_UNDERLINE)
+    no_tags, multi_tags = check_md_tags_headers(vault, mds)
+    
+    row = 1
+    if not (no_tags or multi_tags):
+        stdscr.addstr(row, 1, "No issues found!", colors["GREEN_AND_BLACK"]); row+=1
+
+    if no_tags:
+        stdscr.addstr(row, 1, "No Tags header found in:", colors["GREEN_AND_BLACK"]); row+=1
+        for md in no_tags:
+            stdscr.addstr(row, 1, f" * {md[:-3]}"); row+=1
+
+    if multi_tags:
+        if no_tags:
+            row+=1
+        stdscr.addstr(row, 1, "Multiple Tags headers found in:", colors["GREEN_AND_BLACK"]); row+=1
+        for md in multi_tags:
+            stdscr.addstr(row, 1, f" * {md[:-3]}"); row+=1
+
+    stdscr.addstr(curses.LINES - 3, 1 , "[f]:fix headers")
+    stdscr.addstr(curses.LINES - 2, 1 , "[c]:back")
+    stdscr.addstr(curses.LINES - 2, curses.COLS - 10, "[q]:quit")
+
+    stdscr.refresh()
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "c":
+            disp_md_selection(stdscr, vault, ticked)
+        
+        elif key == "f":
+            disp_fix_headers(stdscr, vault, mds, ticked)
+
+        elif key == "q":
+            quit()
+
+
+def disp_fix_headers(stdscr, vault, mds, ticked):
+    mds = [md for md, tick in zip(mds, ticked) if tick]
+    stdscr.clear()
+    stdscr.addstr(0, 1, 'Current Vault: '+ vault, curses.A_UNDERLINE)
+    no_tags, multi_tags = check_md_tags_headers(vault, mds)
+    
+    row = 1
+    if no_tags:
+        for md in no_tags:
+            write_header(vault, md)
+        
+        stdscr.addstr(row, 1, "Headers added!", colors["YELLOW_AND_BLACK"]); row+=1
+
+    if multi_tags:
+        if no_tags:
+            row+=1
+        stdscr.addstr(row, 1, "Multiple Tags headers found in:", colors["GREEN_AND_BLACK"]); row+=1
+        for md in multi_tags:
+            stdscr.addstr(row, 1, f" * {md[:-3]}"); row+=1
+        row+=1
+        stdscr.addstr(row, 1, "Multi-tag issues must be solved manually", colors['RED_AND_BLACK']); row+=1
+        
+
+    stdscr.addstr(curses.LINES - 2, 1 , "[c]:back")
+    stdscr.addstr(curses.LINES - 2, curses.COLS - 10, "[q]:quit")
+
+    stdscr.refresh()
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "c":
+            disp_md_selection(stdscr, vault, ticked)
+        
+        elif key == "q":
+            quit()
+
+
+def disp_inspect_tags(stdscr, vault, mds, ticked):
+    mds = [md for md, tick in zip(mds, ticked) if tick]
+
+    stdscr.clear()
+    stdscr.addstr(0, 1, 'Current Vault: '+ vault, curses.A_UNDERLINE)
+    
+    row = 0
+    for md in mds:
+        row+=1
+        tags = inspect_md_tags(vault, md)
+        stdscr.addstr(row, 1, f"{md[:-3]}:", colors["CYAN_AND_BLACK"]); row+=1
+        for tag in tags:
+            stdscr.addstr(row, 1, f" * {tag}"); row+=1
+
+    stdscr.addstr(curses.LINES - 2, 1 , "[i]:back to selection")
+    stdscr.addstr(curses.LINES - 2, curses.COLS - 10, "[q]:quit")
+
+    stdscr.refresh()
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "i":
+            disp_md_selection(stdscr, vault, ticked)
+        elif key == "r":
+            remove_tags(stdscr, vault, mds, ticked)
+
+        elif key == "q":
+            quit()
+
+
+def disp_md_selection(stdscr, vault, ticked):
+    stdscr.clear()
+    mds, _ = explore_dir(vault)
+    mds = mds if isinstance(mds, list) else [mds]
+    if ticked is None:
+        ticked = [True for _ in mds]
+    height, width = stdscr.getmaxyx()
+
+    stdscr.addstr(0, 1, 'Current Vault: ' + vault, curses.A_UNDERLINE)
+    stdscr.addstr(1, 1, "Select files to process:", colors['YELLOW_AND_BLACK'])
+    stdscr.refresh()
+    
+    stdscr.nodelay(True)
+    selection = 0
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "KEY_UP":
+            selection-=1
+        elif key == "KEY_DOWN":
+            selection+=1
+
+        elif key == "s":
+            ticked[selection] = not ticked[selection]
+        elif key == "a":
+            if all(ticked):
+                ticked = [False for _ in mds]
+            else:
+                ticked = [True for _ in mds]
+
+        elif key == "c":
+            disp_check_headers(stdscr, vault, mds, ticked)
+
+        elif key == "i":
+            disp_inspect_tags(stdscr, vault, mds, ticked)
+
+        elif key == "t":
+            disp_suggest_tags(stdscr, vault, mds, ticked)
+
+        elif key == "q":
+            quit()
+
+        selection = selection % len(mds)
+        row = 2
+        for i, md in enumerate(mds):
+            pre = '+' if ticked[i] else '-'
+            pre_color = colors['GREEN_AND_BLACK'] if ticked[i] else colors['RED_AND_BLACK']
+            stdscr.addstr(row, 1, pre, pre_color)
+            stdscr.addstr(row, 3, md[:-3], curses.A_STANDOUT if selection == i else colors["CYAN_AND_BLACK"]) 
+            row+=1
+
+        stdscr.addstr(height - 3, (len("[s]:select/deselect [a]:select/deselect all") + 2), "[t]:suggest tags")
+        stdscr.addstr(height - 3, 1, "[c]:check headers")
+        stdscr.addstr(height - 3, len("[s]:select/deselect") + 2, "[i]:inspect tags")
+        stdscr.addstr(height - 2, 1, "[s]:select/deselect")
+        stdscr.addstr(height - 2, len("[s]:select/deselect") + 2, "[a]:select/deselect all")
+        stdscr.addstr(height - 2, width - 10, "[q]:quit")
+        
+        stdscr.refresh()
+
+
+def quit():
+    curses.endwin()
+    exit()
+
+
+stdscr = curses.initscr()
+stdscr.clear()
+stdscr.keypad(True)
+
+curses.curs_set(0)
+curses.noecho()
+curses.cbreak()
+
+colors = dict()
+curses.start_color()
+
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+colors['GREEN_AND_BLACK'] = curses.color_pair(1)
+
+curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+colors['MAGENTA_AND_BLACK'] = curses.color_pair(2)
+
+curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
+colors['CYAN_AND_BLACK'] = curses.color_pair(3)
+
+curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+colors['YELLOW_AND_BLACK'] = curses.color_pair(4)
+
+curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+colors['RED_AND_BLACK'] = curses.color_pair(5)
+
+curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_WHITE)
+colors['GREEN_AND_WHITE'] = curses.color_pair(6)
+
+curses.init_pair(7, curses.COLOR_RED, curses.COLOR_WHITE)
+colors['RED_AND_WHITE'] = curses.color_pair(7)
+
+vault = r'D:\Documents (HD)\Obsidian Vaults\Software'
+disp_md_selection(stdscr, vault, None)
---
+++ utils.py
@@ -0,0 +1,211 @@
+import os
+import re
+import numpy as np
+from typing import List, Union
+
+def explore_dir(path):
+    files = os.listdir(path)
+    md_files = []
+    subdirs = []
+    for file in files:
+        if file.endswith('.md'):
+            md_files.append(file)
+        elif os.path.isdir(os.path.join(path, file)):
+            subdirs.append(file)
+    return md_files, subdirs
+
+
+def check_md_tags_headers(vault_path:str, source: Union[List[str], str]):
+    md_files = source if isinstance(source, list) else [source]
+
+    no_tags = []
+    multi_tags = []    
+    for md_name in md_files:
+        tag_flag = 0
+        with open(os.path.join(vault_path, md_name), 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        for line in text.split('\n'):
+            if line == '###### Tags':
+                tag_flag += 1
+                continue
+
+        if tag_flag == 0:
+            no_tags.append(md_name)
+        elif tag_flag > 1:
+            multi_tags.append(md_name)
+        
+    return no_tags, multi_tags
+
+
+
+def check_tags_headers(vault_path:str, source: Union[List[str], str], explore_subdirs=False, no_tags = [], multi_tags = [], depth = 0):
+    try:
+        files_path = os.path.join(os.getcwd(), source)
+        is_dir = os.path.isdir(files_path)
+    except:
+        is_dir = False
+
+    if  is_dir:
+        md_files, subdirs = explore_dir(source)
+    else:
+        md_files = source if isinstance(source, list) else [source]
+        subdirs = []
+
+    for md_name in md_files:
+        tag_flag = 0
+        with open(os.path.join(vault_path, md_name), 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        for line in text.split('\n'):
+            if line == '###### Tags':
+                tag_flag += 1
+                continue
+
+        if tag_flag == 0:
+            no_tags.append(md_name)
+        elif tag_flag > 1:
+            multi_tags.append(md_name)
+    
+    if explore_subdirs & (subdirs != []):
+        for subdir in subdirs:
+            check_tags_headers(os.path.join(path, subdir), explore_subdirs, no_tags, multi_tags, depth+1)
+    
+    if depth == 0:
+        if no_tags:
+            print(f"No tags header found in:")
+            for md_name in no_tags:
+                print(f" - {md_name[:-3]}")
+            print("Make sure these are under '###### Tags' header.")
+
+        if multi_tags:
+            print(f"Multiple tags headers found in:")
+            for md_name in multi_tags:
+                print(f" - {md_name[:-3]}")
+        
+        return no_tags, multi_tags
+
+
+def inspect_tags(path):
+    md_files, subdirs = explore_dir(path)
+    tagset = set()
+    for md_name in md_files:
+        tag_flag = False
+        with open(os.path.join(path, md_name), 'r', encoding='utf-8') as f:
+            text = f.read()
+        
+        for line in text.split('\n'):
+            if line == '###### Tags':
+                tag_flag = True
+                continue
+            if tag_flag:
+                tags = re.findall(r'#([^ ]+)', line)
+                if tags:
+                    tagset = tagset.union(set(tags))
+                else:
+                    break
+
+    tagset = sorted(list(tagset))
+    for tag in tagset:
+        print(tag)
+
+
+def inspect_md_tags(vault_path:str, md: Union[List[str], str]):
+    
+    tagset = set()
+    tag_flag = False
+    with open(os.path.join(vault_path, md), 'r', encoding='utf-8') as f:
+        text = f.read()
+    
+    for line in text.split('\n'):
+        if tag_flag:
+            tags = re.findall(r'#([^ ]+)', line)
+            if tags:
+                tagset = tagset.union(set(tags))
+            else:
+                break
+
+        else:
+            if line == '###### Tags':
+                tag_flag = True
+                continue
+
+    return sorted(list(tagset))
+
+
+def tags_relevance(path):
+    md_files, subdirs = explore_dir(path)
+    
+    tagmultiset = dict()
+    for md in md_files:
+        with open(os.path.join(path, md), 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        tag_flag = False
+        for line in text.split('\n'):
+            if line == '###### Tags':
+                tag_flag = True
+                continue
+            if tag_flag:
+                tags = re.findall(r'#([^ ]+)', line)
+                if tags:
+                    for tag in tags:
+                        tagmultiset[tag] = tagmultiset.get(tag, 0)*1.2 + np.log(len(text))
+
+    tagmultiset = dict(sorted(tagmultiset.items(), key=lambda item: item[1], reverse=True))
+    for tag, count in tagmultiset.items():
+        print(f'{tag}:{count}')
+
+
+def demo_suggest_tags(vault, md, n_suggestions):
+    from numpy.random import default_rng
+    rng = default_rng()
+    existing_tags = inspect_md_tags(vault, md) 
+    new_tags = []
+
+    while len(new_tags)<n_suggestions:
+        tag_ids = rng.integers(0, 10, n_suggestions)
+        sug_tags = [f'Tag{id}' for id in tag_ids]
+        for tag in sug_tags:
+            if (tag not in existing_tags) and (tag not in new_tags):
+                new_tags.append(tag)
+                if len(new_tags) == n_suggestions:
+                    break
+    return new_tags
+
+
+def write_header(vault = 'Software', md = 'test.md'):
+    target = os.path.join(vault, md)
+    with open(target, 'a') as file:
+        file.write('\n###### Tags\n')
+
+
+def write_tags(vault = 'Software', md = 'test.md', tags = ['bruh', 'nice']):
+    target = os.path.join(vault, md)
+    with open(target, 'r') as file:
+        text = file.readlines()
+        for line in text:
+                pass
+        if line[-1:]!='\n':
+            md.write('\n')         
+    
+    with open(target, 'a') as md:
+        hash_tagged = [f'#{tag} ' for tag in tags]
+        md.write(str().join(hash_tagged))
+
+#TODO: find way to not store the whole file in memory using IO stream stuff
+def remove_tags(vault, md, tags_to_remove):
+    target = os.path.join(vault, md)
+    with open(target, 'r+', encoding = 'utf-8') as file:
+        line = file.readline()
+        while line:
+            if line == '###### Tags\n':
+                tag_loc = file.tell()
+                tags = file.read()
+                for tag in tags_to_remove:
+                    tags = tags.replace(tag, '').strip()
+                file.seek(tag_loc)
+                file.write(tags)
+                file.truncate(file.tell())
+                break
+            line = file.readline()
